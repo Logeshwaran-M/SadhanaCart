cf9e6f84f24e752c587248ccffba5f532708305d
LOGESHWARAN <logeshwaran83563@gmail.com>
2026-04-04 17:42:27 +0530

    Update send-order-email.js

--- api/send-order-email.js
+++ api/send-order-email.js
@@ -24,9 +24,7 @@ export default async function handler(req, res) {
 
     // ✅ MAIL CONFIG (Zoho / Gmail both supported)
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com", // or smtp.gmail.com
-      port: 465,
-      secure: true,
+      service: "gmail",
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
